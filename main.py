3c3c2e263fa880b450b7b9f94cfa3a43717e7937
AbsurdCactus <braydonreber@gmail.com>
2021-11-15 07:24:25 -0500

    Brought over from Palindrome-Permutation-Python

---
+++ main.py
@@ -0,0 +1,34 @@
+
+def palindrome_detector(string_one, string_two):
+
+    string_list = [string_one, string_two]
+    longest = max(string_list)
+    shortest = min(string_list)
+
+    char_change_counter = 0
+
+    if len(longest) - len(shortest) > 1:
+
+        char_change_counter += 1
+
+    
+
+    for i in longest:
+
+        if i not in shortest:
+
+            char_change_counter += 1
+
+    for i in shortest:
+
+        if i not in longest:
+
+            char_change_counter += 1
+
+    if char_change_counter > 1:
+
+        return False
+
+    else:
+
+        return True
---
+++ test.py
@@ -0,0 +1,65 @@
+import unittest
+import main
+
+
+class TestMain(unittest.TestCase):
+
+    def test_pale_ple(self):
+
+        self.assertEqual(main.palindrome_detector(
+            string_one="pale", string_two="ple"), True)
+
+    def test_pale_palest(self):
+
+        self.assertEqual(main.palindrome_detector(
+            string_one="pale", string_two="palest"), False)
+
+    def test_pale_pa(self):
+
+        self.assertEqual(main.palindrome_detector(
+            string_one="pale", string_two="pa"), False)
+
+    def test_pa_pale(self):
+        self.assertEqual(main.palindrome_detector(
+            string_one="pa", string_two="pale"), False)
+
+    def test_pale_bale(self):
+        self.assertEqual(main.palindrome_detector(
+            string_one="pale", string_two="bale"), True)
+
+    def test_pale_bane(self):
+
+        self.assertEqual(main.palindrome_detector(
+            string_one="pale", string_two="bane"), False)
+
+
+    def test_pales_pale(self):
+
+        self.assertEqual(main.palindrome_detector(
+            string_one="pales", string_two="pale"), True)
+
+    def test_pale_bake(self):
+
+        self.assertEqual(main.palindrome_detector(
+            string_one="pale", string_two="bake"), False)
+
+    def test_pale_ble(self):
+
+        self.assertEqual(main.palindrome_detector(
+            string_one="pale", string_two="ble"), False)
+
+    def test_aaaa_aaa(self):
+
+        self.assertEqual(main.palindrome_detector(
+            string_one="aaaa", string_two="aaa"), True)
+
+    def test_aaaa_aab(self):
+# if shortest not in longest
+        self.assertEqual(main.palindrome_detector(
+            string_one="aaaa", string_two="aab"), False)
+
+
+
+    #there need to be a lot more tests
+
+
